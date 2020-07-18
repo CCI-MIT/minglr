6c84aa4e265182cbea906ba9b7a8a7c6ef23a0a4
Jaeyoon Song <jyo3on@gmail.com>
2020-07-18 17:18:21 +0900

    update consent stmt

--- client/src/components/pages/Consent.js
+++ client/src/components/pages/Consent.js
@@ -13,7 +13,7 @@ function Consent(props) {
     
     let returnThis = <div className="login-container">
         <h3><br/>Consent statement for using MINGLR</h3>
-        <p className="consent">Use of the MINGLR system is part of research being conducted by researchers at Massachusetts Institute of Technology (MIT) and Northeastern University to understand how different modes of communication can be supported in online groups. Your use of the system is voluntary, you may decline to answer any or all questions, and you can stop at any time. The system will record who you talk to and when but not the contents of your conversations. When the results of the research are made public, no information will be included that would reveal your identity.</p>
+        <p className="consent">Use of the MINGLR system is part of research being conducted by researchers at Massachusetts Institute of Technology (MIT) and Northeastern University to understand how different modes of communication can be supported in online groups. Your use of the system is voluntary, you may decline to answer any or all questions, and you can stop at any time. The system will record who you talk to and when but not the contents of your conversations. You may be contacted by email with surveys or other questions from the researchers. When the results of the research are made public, no information will be included that would reveal your identity.</p>
         <p>I consent to participate in this research by using the MINGLR system:</p>
         <div>
             <span className="btn" onClick={() => handleAnswer("YES")}>Yes, I do</span>

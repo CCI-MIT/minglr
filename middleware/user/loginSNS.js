609870a445060c00a655910ecdcb50c13d09b8fc
Jaeyoon Song <jyo3on@gmail.com>
2020-08-04 00:20:21 +0900

    sns login fixed

--- middleware/user/loginSNS.js
+++ middleware/user/loginSNS.js
@@ -7,7 +7,28 @@ let loginSNS = (req, res, next) => {
     try {
         User.findOne({ email: req.body.email }, (err, user) => {
             if (!user) {
-                return createUser(req, res, next);
+                let index = 0
+                User.findOne().sort({$natural: -1}).limit(1).exec(function(err, last){
+                    index = last.id + 1;
+                    const user = new User({
+                        firstname: req.body.firstname,
+                        lastname: req.body.lastname,
+                        email: req.body.email,
+                        id: index,
+                    });
+
+                    const token = user.activate(req.body.type);
+                    res.cookie("w_auth", token, { maxAge: 2 * 60 * 60 * 1000, httpOnly: true })
+                        .status(200)
+                        .json({
+                            success: true,
+                            type: "SIGNUP",
+                            _id: user._id.toString(),
+                        });
+                    
+                    res.locals.user = user;
+                    next();
+                });
             }
             else {
                 res.locals.user = user;
